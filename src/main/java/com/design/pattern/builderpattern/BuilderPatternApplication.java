b166fe3b21b30159a9e2bb4d6b0bcdcc5b2d80e8
bilal <bilal.ilyas@mobilelive.ca>
2018-04-21 23:04:53 +0500

    archtype builder pattern

--- src/main/java/com/design/pattern/builderpattern/BuilderPatternApplication.java
+++ src/main/java/com/design/pattern/builderpattern/BuilderPatternApplication.java
@@ -15,7 +15,7 @@ public class BuilderPatternApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BuilderPatternApplication.class, args);
         System.out.println();
-		System.out.println("Starting Spring-Boot Application: Filter Design Pattern...!");
+		System.out.println("Starting Spring-Boot Application: Builder Design Pattern...!");
 
 
 	}
